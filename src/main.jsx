c3774e213b2cce22731ad2466b729c66f5480422
Tommy <azran.thomas@gmail.com>
2025-06-13 13:17:54 -0400

    changing croll issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,28 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import ScrollToTop from "./components/ScrollToTop";
-import App from "./App.jsx";
-import Projects from "./pages/Projects.jsx";
-import NotFoundError from "./pages/NotFoundError.jsx";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+import App from './App.jsx'
+import Projects from './pages/Projects.jsx'
+import NotFoundError from './pages/NotFoundError.jsx';
+import { path } from 'framer-motion/client';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
     errorElement: <NotFoundError />,
   },
   {
-    path: "/projects",
+    path: '/projects',
     element: <Projects />,
     errorElement: <NotFoundError />,
   },
-]);
+])
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <>
-      <ScrollToTop />
-      <RouterProvider router={router} />
-    </>
-  </StrictMode>
-);
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
