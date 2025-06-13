a956a3ff905ed2f9ebec62cb1713ce816407d541
Tommy <azran.thomas@gmail.com>
2025-06-13 13:12:27 -0400

    fixing scrrolling issue oon link click

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
-import Projects from './pages/Projects.jsx'
-import NotFoundError from './pages/NotFoundError.jsx';
-import { path } from 'framer-motion/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
+import App from "./App.jsx";
+import Projects from "./pages/Projects.jsx";
+import NotFoundError from "./pages/NotFoundError.jsx";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     errorElement: <NotFoundError />,
   },
   {
-    path: '/projects',
+    path: "/projects",
     element: <Projects />,
     errorElement: <NotFoundError />,
   },
-])
+]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <>
+      <ScrollToTop />
+      <RouterProvider router={router} />
+    </>
+  </StrictMode>
+);
